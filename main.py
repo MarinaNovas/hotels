98675e1d5f92ad5496f58e5c310c4028f1f044c4
MarinaNovas <marismirnovastar@gmail.com>
2026-01-02 14:46:47 +0300

    add delete endpoint

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, Query
 import uvicorn
 
-from main_ import hotels
-
 app = FastAPI()
 hotels = [
     {"id": 1, "title": "Sochi"},
@@ -37,6 +35,12 @@ def get_hotels(id:int |None = Query(None, description="Айдишник"), title
         hotels_sort.append(hotel)
     # return [hotel for hotel in hotels if hotel["title"] == title and hotel["id"] == id]
     return hotels_sort
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != id]
+    return {"SUCCESS": "OK"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
