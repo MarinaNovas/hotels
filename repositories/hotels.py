141e2493670b8c8e4d8bc06e0f7f2880f5ec142a
MarinaNovas <marismirnovastar@gmail.com>
2026-01-10 18:08:19 +0300

    Доработка get hotel

--- repositories/hotels.py
+++ repositories/hotels.py
@@ -3,7 +3,6 @@ from sqlalchemy import select, func, insert
 from repositories.base import BaseRepository
 from src.database import engine
 from src.models.hotels import HotelsOrm
-from src.schemas.hotels import Hotel
 
 class HotelsRepository(BaseRepository):
     model = HotelsOrm
@@ -17,9 +16,9 @@ class HotelsRepository(BaseRepository):
     ):
         query = select(self.model)
         if title:
-            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
+            query = query.filter(func.lower(self.model.title).contains(title.strip().lower()))
         if location:
-            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+            query = query.filter(func.lower(self.model.location).contains(location.strip().lower()))
 
         query = query.limit(limit).offset(offset)
         print(query.compile(engine, compile_kwargs={'literal_binds': True}))
